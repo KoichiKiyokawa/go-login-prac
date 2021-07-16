f0e3458f1846a05749b819b012de622be7ea3021
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-16 13:57:19 +0900

    test: refactor

--- src/service/auth_test.go
+++ src/service/auth_test.go
@@ -11,18 +11,18 @@ type dummyUserRepository struct{}
 
 var dummyUser = entity.User{ID: 1, Email: "test@example.com", Name: "user1", Password: "testtest"}
 
-func (r dummyUserRepository) FindByEmail(email string) (entity.User, error) {
+func (dummyUserRepository) FindByEmail(email string) (entity.User, error) {
 	var encryptedPassword, _ = bcrypt.GenerateFromPassword([]byte(dummyUser.Password), 10)
 	dummyUser.Password = string(encryptedPassword)
 	return dummyUser, nil
 }
-func (r dummyUserRepository) Find(id int) (entity.User, error) {
+func (dummyUserRepository) Find(id int) (entity.User, error) {
 	return entity.User{}, nil
 }
-func (r dummyUserRepository) All() ([]entity.User, error) {
+func (dummyUserRepository) All() ([]entity.User, error) {
 	return []entity.User{}, nil
 }
-func (r dummyUserRepository) Create(user entity.User) (entity.User, error) {
+func (dummyUserRepository) Create(user entity.User) (entity.User, error) {
 	return entity.User{}, nil
 }
 
