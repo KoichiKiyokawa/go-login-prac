8d1799433f52bfda3c20a0c279d22df03aea9125
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-17 17:54:27 +0900

    chore: delete log

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,13 +1,11 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
 )
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("cors")
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == http.MethodOptions {
